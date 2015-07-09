51ccb5cda73f6102fee666fe4bd833075f1f8699
Samuel Rodriguez Bernabeu <srodrb@gmail.com>
2015-07-09 10:38:05 +0200

    Intel ELL kernel sketch

--- inc/kernels.h
+++ inc/kernels.h
@@ -22,7 +22,7 @@ enum ARCHICTECTURE {XEON, KNC};
 
 template <int ARCH, typename FORMAT, typename PREC> void spmv( FORMAT* M) { /*   */};
 
-template <> void spmv <XEON, CSR, float> (CSR* M)
+template <> void spmv <XEON, CSR, float> (CSR* M, Vector* X, Vector* B)
 {
     char trans = 'n';
     char matdescra[6];
@@ -31,5 +31,29 @@ template <> void spmv <XEON, CSR, float> (CSR* M)
     matdescra[2] = 'n';
     matdescra[3] = 'c';
 
-    mkl_zcsrmv( &trans, &A->nrows, );
+    // mkl_zcsrmv( &trans, &A->nrows, );
+};
+
+template <> void spmv <XEON,ELL,float> (ELL* M, Vector* X, Vector* B)
+{
+
+    // aqui tengo que poner todo el rollo de alineamiento
+    __assume_aligned( ELL->ell_sreal, 32);
+    __assume_aligned( ELL->ell_simag, 32);
+    
+    __assume_aligned( ELL->rhs_sreal, 32);
+    __assume_aligned( ELL->rhs_sreal, 32);
+    
+    __assume_aligned( X->sreal, 32);
+    __assume_aligned( X->simag, 32);
+
+    __assume_aligned( B->sreal, 32);
+    __assume_aligned( B->simag, 32);
+
+
+    const size_t rows    = M->ell_rows;
+    const size_t cols    = M->ell_cols;
+    const size_t ell_len = M->ell_len;
+
+
 };
